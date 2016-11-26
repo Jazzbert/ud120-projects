d1efc5aacb8d1f8e0681c47fd27b382fa1c0ed29
Craig Cleveland <craig.cleveland@gmail.com>
2016-11-26 14:01:45 -0500

    Basline Config

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -10,40 +10,61 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary', 'total_payments'] # You will need to use more features
+features_list = ['poi', 'salary'] #'total_payments', 'exercised_stock_options', \
+                 #'deferred_income', 'deferral_payments'] 
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-### Show base metrics on each feature
-full_list = ["salary", "to_messages", "deferral_payments", "total_payments", "loan_advances", "bonus", "restricted_stock_deferred", "deferred_income", "total_stock_value", "expenses", "from_poi_to_this_person", "exercised_stock_options", "from_messages", "other", "from_this_person_to_poi", "poi", "long_term_incentive", "shared_receipt_with_poi", "restricted_stock", "director_fees"]
-for feat in full_list:
-    feat_test = ['poi', feat]
-    data = featureFormat(data_dict, feat_test, sort_keys = True)
-    labels, features = targetFeatureSplit(data)
-    print feat, "values:", len(features), " percent:", float(len(features))/float(len(data_dict))
-
 ### Task 2: Remove outliers
-
+data_dict.pop("TOTAL",0)
 
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
+## TODO: cycle through data set to add new feature net_personal_inflow
+
+### Show base metrics on each feature
+##full_list = ["salary", "to_messages", "deferral_payments", "total_payments", "loan_advances", "bonus", "restricted_stock_deferred", "deferred_income", "total_stock_value", "expenses", "from_poi_to_this_person", "exercised_stock_options", "from_messages", "other", "from_this_person_to_poi", "poi", "long_term_incentive", "shared_receipt_with_poi", "restricted_stock", "director_fees"]
+##for feat in full_list:
+##    feat_test = ['poi', feat]
+##    data = featureFormat(data_dict, feat_test, sort_keys = True)
+##    labels, features = targetFeatureSplit(data)
+##    print feat, "values:", len(features), " percent:", float(len(features))/float(len(data_dict))
+##
+##cols = len(data_dict)
+##print "cols:", cols
+##for x in range(len(cols)):
+##    print "items in col", x, len(data_dict[x])
+
+### Add together the total payments and exercised options for a estimate of net "cash out"
+
+
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
-import matplotlib.pyplot
-for point in data:
-    salary = point[1]
-    total_payments = point[2]
-    matplotlib.pyplot.scatter(salary, total_payments)
-
-matplotlib.pyplot.xlabel("salary")
-matplotlib.pyplot.ylabel("total_payments")
-matplotlib.pyplot.show()
+### Code for visualizeing and looking a different data characteristics
+##biggest = 0
+##
+##import matplotlib.pyplot
+##for point in data:
+##    salary = point[1]
+##    total_payments = point[2]
+##    matplotlib.pyplot.scatter(salary, total_payments)
+##    vector = salary + total_payments
+##    if vector > biggest:
+##        biggest = vector
+##        biggestPayment = point[1]
+##        biggestExercise = point[2]
+##
+##print biggestPayment, biggestExercise
+##
+##matplotlib.pyplot.xlabel("salary")
+##matplotlib.pyplot.ylabel("options_exericised")
+##matplotlib.pyplot.show()
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -67,6 +88,10 @@ from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf.fit(features_train, labels_train)
+scr = clf.score(features_test, labels_test)
+print scr
+
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
@@ -75,48 +100,4 @@ features_train, features_test, labels_train, labels_test = \
 dump_classifier_and_data(clf, my_dataset, features_list)
 
 
-### Outlier cleaner modified from previous lesson
-def outlierCleaner(predictions, ages, net_worths):
-    """
-        Clean away the 10% of points that have the largest
-        residual errors (difference between the prediction
-        and the actual net worth).
-
-        Return a list of tuples named cleaned_data where 
-        each tuple is of the form (age, net_worth, error).
-    """
-    
-    cleaned_data = []
-
-    ### your code goes here
-
-    size = len(predictions)
-    cleanSize = int(size * .1)
-    bigError = [0] * cleanSize
-    
-    print "size: ", size
-    print "cleanSize", cleanSize
-
-    for x in range(0, size):
-
-        errorVal = abs(predictions[x] - net_worths[x]) 
-        
-        # set value of cleaned data
-        cleaned_data.append((ages[x], net_worths[x], errorVal))
-
-        # keep track of the 10 largest error values
-        if errorVal >= bigError[0]:
-            bigError[0] = errorVal
-            bigError = sorted(bigError)
-
-    print bigError
-
-    # remove biggest errors from list
-    for y in range (0, cleanSize):
-        for z in xrange (0, len(cleaned_data)):
-            if cleaned_data[z][2] == bigError[y]:
-                del cleaned_data[z]
-                break
-    
-    return cleaned_data
 
